8b4c4750395e3183408052df8c2aa855399d99c1
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2021-03-13 17:35:25 +0300

    Added example files.

---
+++ example/hinteg2d.jl
@@ -0,0 +1,18 @@
+# This file includes and example file for 2D integral
+
+using FractalTools 
+using GeometryBasics
+using Makie 
+
+# Generate data 
+f(x, y) = [
+    x^2 + y^2 + 1, 
+    x^2 - y^2 + 1, 
+]
+ngon = Triangle(
+    Point(BigFloat(0.), BigFloat(0.)), 
+    Point(BigFloat(1.), BigFloat(0.)), 
+    Point(BigFloat(0.5), BigFloat(1.)))
+npts = 100
+pts = getdata(f, ngon, npts)
+interp = integrate(pts, Interp2D(0.001))
