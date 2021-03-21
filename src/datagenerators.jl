900495cda072485fe3e1b13d679de0adc8e39bb1
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2021-03-21 19:48:10 +0300

    Fixed Test `getdata` function returns correct number of points. #37

--- src/datagenerators.jl
+++ src/datagenerators.jl
@@ -13,15 +13,32 @@ Returns a vector of (d + m)-dimensional points that are dispersed randomly over 
 Returns a vector of d-dimensional points that are dispersed randomly over d-dimensioanl domain with vertex points
 `vtx`. `npts` is the number of points to be returned` 
 """
-getdata(f, vtx::AbstractVector{<:AbstractVector}, npts::Int) = getdata(f, ngon(vtx), npts)
-getdata(vtx::AbstractVector{<:AbstractVector}, npts::Int)    = getdata(ngon(vtx), npts)
-getdata(f, domain::Ngon, npts::Int) = [Point(pnt..., f(pnt...)...) for pnt in disperse(domain, npts)] 
-getdata(domain::Ngon, npts::Int)    = disperse(domain, npts)
+getdata(f, vtx::AbstractVector{<:AbstractVector}, nedgepoints::Int=10, ninterpoints::Int=10) = 
+    getdata(f, ngon(vtx), nedgepoints, ninterpoints)
+getdata(vtx::AbstractVector{<:AbstractVector}, nedgepoints::Int=10, ninterpoints::Int=10) = 
+    getdata(ngon(vtx), nedgepoints, ninterpoints)
+getdata(f, domain::Ngon, nedgepoints::Int=10, ninterpoints::Int=10) = 
+    [Point(pnt..., f(pnt...)...) for pnt in disperse(domain, nedgepoints, ninterpoints)] 
+getdata(domain::Ngon, nedgepoints::Int=10, ninterpoints::Int=10) = 
+    disperse(domain, nedgepoints, ninterpoints)
+
+# For backward compatibility. 
+
+function getdata(f, vtx, npoints::Int)
+    msg = "`getdata(f, vtx, npoints) is deprecated. Use `getdata(f, vtx, nedgepoints, ninterpoints)`"
+    msg *= "The nedgepoints is set to 10 and ninterpoints is set to $npoints"
+    warn(msg) 
+    getdata(f, vtx, 10, npoints) 
+end 
+
+function getdata(vtx, npoints::Int)
+    msg = "`getdata(vtx, npoints) is deprecated. Use `getdata(vtx, nedgepoints, ninterpoints)`"
+    msg *= "The nedgepoints is set to 10 and ninterpoints is set to $npoints"
+    warn(msg) 
+    getdata(f, vtx, 10, npoints) 
+end 
 
 ngon(pts::AbstractVector{<:AbstractVector}) = Ngon(SVector{length(pts)}(map(item -> Point(item...), pts)))
-
-# TODO: Test `getdata` function with `Line` domains.
-# FIXME: #37 Test `getdata` function returns correct number of points.
 
 """
     getpoint(ngon::Ngon, maxiter::Int=100_000) 
@@ -44,7 +61,7 @@ function getpoint(ngon::Ngon{Dim, T, N, P}; maxiter::Int=100_000) where {Dim, T,
         isvalidpoint(pnt, tess) && return pnt
         iter += 1
     end 
-    return Point(NaN, NaN)  # For type-stability 
+    return Point(NaN, NaN) 
 end 
 
 function getpoint(line::Line{1, T}) where {T}
@@ -53,26 +70,32 @@ function getpoint(line::Line{1, T}) where {T}
     Point(pnt)
 end 
 
-boundarypoints(line::Line; numpoints::Int=10) = linepoint(line[1], line[2])
-boundarypoints(ngon::Ngon; numpoints::Int=10) = vcat(
-        map(
-            ((pnt1, pnt2),) -> linepoint(pnt1, pnt2), TupleView{2, 1}(SVector([ngon.points; [ngon.points[1]]]...))
-            )...
-        )
-
-function linepoint(p1::AbstractPoint, p2::AbstractPoint; numpoints::Int=10) 
-    x = collect(LinRange(p1[1], p2[1], numpoints))
-    y = collect(LinRange(p1[2], p2[2], numpoints))
-    [Point(xi, yi) for (xi, yi) in zip(x, y)]
-end
-
 isvalidpoint(pnt::AbstractPoint, tess) = tess.find_simplex(pnt)[1] ≥ 0 && pnt !== Point(NaN, NaN)
 
-function disperse(ngon::Ngon, npoints::Int) 
-    allpnts = [getpoint(ngon) for i in 1 : 10 * npoints]
-    ctrpnts = [Point(val[1], val[2]) for val in eachcol(kmeans(hcat(collect.(allpnts)...), npoints).centers)]
-    vcat(ngon.points, boundarypoints(ngon), ctrpnts)
+
+function disperse(ngon::Ngon, nedgepoints::Int, ninterpoints::Int) 
+    interpoints = interiorpoints(ngon, ninterpoints)
+    boundpoints = boundarypoints(ngon, nedgepoints)
+    vcat(boundpoints, interpoints) |> unique
 end
+disperse(line::Line, nedgepoints::Int, ninterpoints::Int) = boundarypoints(line, nedgepoints)
+
+function interiorpoints(ngon::Ngon, ninterpoints::Int)
+    allpnts = [getpoint(ngon) for i in 1 : 10 * ninterpoints]
+    [Point(val[1], val[2]) for val in eachcol(kmeans(hcat(collect.(allpnts)...), ninterpoints).centers)]
+end 
+
+boundarypoints(line::Line, nedgepoints::Int=10) = edgepoints(line[1], line[2], nedgepoints)
+boundarypoints(ngon::Ngon, nedgepoints::Int=10) = vcat(map(((pnt1, pnt2),) -> edgepoints(pnt1, pnt2, nedgepoints), 
+    TupleView{2, 1}(SVector([ngon.points; [ngon.points[1]]]...)))...)
+
+function edgepoints(p1::AbstractPoint{2, T}, p2::AbstractPoint{2, T}, nedgepoints::Int=10) where {T}
+    x = collect(LinRange(p1[1], p2[1], nedgepoints))
+    y = collect(LinRange(p1[2], p2[2], nedgepoints))
+    [Point(xi, yi) for (xi, yi) in zip(x, y)]
+end
+edgepoints(p1::AbstractPoint{1, T}, p2::AbstractPoint{1, T}, nedgepoints::Int=10) where {T} = 
+    [Point(xi) for xi in collect(LinRange(p1[1], p2[1], nedgepoints))]
 
 function boundboxtransforms(ngon::Ngon)
     coords = coordinates(ngon) 
