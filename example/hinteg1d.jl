0504bb34bfae566a3f6818bd3955c3c9f1dcc68a
zekeriya.sari <zekeriya.sari@deu.edu.tr>
2021-03-23 12:25:44 +0300

    Intermediate commit.

--- example/hinteg1d.jl
+++ example/hinteg1d.jl
@@ -4,9 +4,12 @@ using FractalTools
 using Makie 
 
 # Generate data 
-xi, dx, xf = 0., 0.01, 10.
-x = collect(xi : dx : xf) 
-y = sin.(x) 
-z = cos.(x) 
-pts = collect.(zip(x, y, z))
+line = [[0.], [10.]]
+f(x) = [sin(x), cos(x)]
+pts = getdata(f, line, 1001)
+# xi, dx, xf = 0., 0.01, 10.
+# x = collect(xi : dx : xf) 
+# y = sin.(x) 
+# z = cos.(x) 
+# pts = collect.(zip(x, y, z))
 res = integrate(pts, HInterp1D(0.001 * ones(2, 2)))
