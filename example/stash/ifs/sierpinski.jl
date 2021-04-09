f9e4dd874bbaa79fc976fb4dd4906770363bc07f
gizem.kalender <gizekalender.eee@gmail.com>
2021-04-09 15:20:34 +0300

    Started to implement nonallocated attractor construction.

--- example/stash/ifs/sierpinski.jl
+++ example/stash/ifs/sierpinski.jl
@@ -4,9 +4,9 @@ using FractalTools
 using Plots
 
 # Construct ifs 
-ifs = Fern() 
+ifs = Sierpinski() 
 initset = [rand(2)]
-atr = attractor(ifs, initset, alg=DetAlg(), numiter=5) 
+atr = attractor(ifs, initset, alg=RandAlg(), numiter=1000, allocated=false) 
 
-# Plot the attractor 
-scatter(getindex.(atr.set,1), getindex.(atr.set,2), markersize=3)
+# # Plot the attractor 
+# scatter(getindex.(atr.set,1), getindex.(atr.set,2), markersize=3)
