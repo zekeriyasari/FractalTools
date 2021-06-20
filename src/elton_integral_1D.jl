78ec67c054c302f6962dcbe5fef4d208d08ba1c4
gizem.kalender <gizekalender.eee@gmail.com>
2021-06-20 13:43:50 +0300

    20_06_21_update

--- src/elton_integral_1D.jl
+++ src/elton_integral_1D.jl
@@ -1,14 +1,14 @@
 using FractalTools
 using LinearAlgebra
 
-export elton_integral, mean
+export elton_integral_1D, mean
 #ToDo: Assume that indicator is 1 for one version
 # ToDo : Elton integral function and norm function must run vector.
 function mean(x)
     sum(x) / length(x)
 end
 
-function elton_integral(func, ind_func, points::Channel, func_params=tuple(), ind_params=tuple(), ϵ::Real=1e-10, max_iter=1e6, chunk_size = 10)
+function elton_integral_1D(func, ind_func, points::Channel, func_params=tuple(), ind_params=tuple(), ϵ::Real=1e-10, max_iter=1e6, chunk_size = 10)
     number_of_points = 0
     old_number_of_points = 0
     result = 0
@@ -29,7 +29,7 @@ function elton_integral(func, ind_func, points::Channel, func_params=tuple(), in
         end
         filtered_set_of_points = set_of_points[indicator.>0] 
         number_of_points += number_of_indicator
-        val =  func(filtered_set_of_points, func_params...) .* indicator[indicator .> 0]
+        val =  func.(filtered_set_of_points, func_params...) .* indicator[indicator .> 0]
         result = (1 / (number_of_points +1)) * ((old_number_of_points + 1) * old_result + sum(val))
         Δ[mod(k,chunk_size)+1] = abs(result - old_result) 
         k += 1
@@ -38,7 +38,7 @@ function elton_integral(func, ind_func, points::Channel, func_params=tuple(), in
 end
 
 
-function elton_integral(func, points::Channel, func_params=tuple(), ϵ::Real=1e-10, max_iter=1e6, chunk_size = 10)
+function elton_integral_1D(func, points::Channel, func_params=tuple(), ϵ::Real=1e-10, max_iter=1e6, chunk_size = 10)
     number_of_points = 0
     old_number_of_points = 0
     result = 0
@@ -50,7 +50,7 @@ function elton_integral(func, points::Channel, func_params=tuple(), ϵ::Real=1e-
         old_number_of_points = number_of_points
         old_result = result
         set_of_points = take!(points)
-        val =  func(set_of_points, func_params...) 
+        val =  func.(set_of_points, func_params...) 
         number_of_points += chunk_size
         result = (1 / (number_of_points +1)) * ((old_number_of_points + 1) * old_result + sum(val))
         Δ[mod(k,chunk_size)+1] = abs(result - old_result) 
